67131fadb65127b8c03f26012be26aa7eedc65fa
AlberMix <168641251+AlberMix@users.noreply.github.com>
2024-05-01 14:32:21 +0200

    Add files via upload

--- JavaApplication6.java
+++ JavaApplication6.java
@@ -15,7 +15,7 @@ public class JavaApplication6 {
      */
     public static void main(String[] args) {
 
-        System.out.println("hola");
+        System.out.println("hola Mundo");
     }
     
 }
